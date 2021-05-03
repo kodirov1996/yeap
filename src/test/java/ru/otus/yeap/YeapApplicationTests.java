c8a1ca29eb77d825b626d40e78f17ae9c6ebcf56
adhamsulton <adhamsulton@gmail.com>
2021-05-03 10:18:23 +0500

    upload

---
+++ src/test/java/ru/otus/yeap/YeapApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.yeap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
